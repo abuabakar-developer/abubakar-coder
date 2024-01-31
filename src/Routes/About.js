a57a9fcce9058ade3078df62a7767b65519c9835
abuabakar-developer <157199734+abuabakar-developer@users.noreply.github.com>
2024-01-30 23:14:35 -0800

    add Front.js and FrontStyles.css

---
+++ src/Routes/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from '../Components/Navbar';
+import Info from '../Components/Info';
+import Front from '../Components/Front'
+import Footer from '../Components/Footer';
+
+const About = () => {
+  return (
+    <div>
+       <Navbar />
+      <Info />
+      <Front />
+      <Footer />
+    </div>
+  );
+};
+ 
+export default About;
